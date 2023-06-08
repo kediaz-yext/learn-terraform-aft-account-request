5d0838337d22f6d4e18f6c8719ffac7c74bd99a6
Kevin Diaz <kediaz@yext.com>
2023-06-08 13:39:36 -0400

    Create aft test account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "aft_test" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "kediaz+afttest@yext.com"
+    AccountName               = "aft-test"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "kediaz+afttest@yext.com"
+    SSOUserFirstName          = "aft"
+    SSOUserLastName           = "test"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Kevin Diaz"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
